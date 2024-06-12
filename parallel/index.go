b825a6e33000e9286b14bbd8eb1392f0b9f07f54
Keith <keith.mackay@imaginexdigital.com>
2024-06-12 12:28:09 -0400

    Add Mapping function

--- parallel/index.go
+++ parallel/index.go
@@ -1,7 +1,9 @@
 package parallel
 
 import (
+	"context"
 	"golang.org/x/sync/errgroup"
+	"golang.org/x/sync/semaphore"
 )
 
 func ForEach[T any](list []T, threads int, action func(t T) error) error {
@@ -14,4 +16,23 @@ func ForEach[T any](list []T, threads int, action func(t T) error) error {
 		})
 	}
 	return eg.Wait()
+}
+
+func Map[T any, S any](list []T, threads int, action func(t T) S) ([]S, error) {
+	mappedValues := make([]S, len(list))
+	sem := semaphore.NewWeighted(1)
+	var eg errgroup.Group
+	eg.SetLimit(threads)
+	for index, item := range list {
+		i := item
+		x := index
+		eg.Go(func() error {
+			val := action(i)
+			defer sem.Release(1)
+			_ = sem.Acquire(context.Background(), 1)
+			mappedValues[x] = val
+			return nil
+		})
+	}
+	return mappedValues, eg.Wait()
 }
